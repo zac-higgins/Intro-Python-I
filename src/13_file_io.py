b3da88bb4da06b7b03c4305721f81d7031fa6d73
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-03-10 10:02:41 -0600

    completed challenges 11 through 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
 
+def readFoo():
+    with open("foo.txt") as text:
+        print(text.read())
+
+
+readFoo()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+
+def writeBar():
+    with open("bar.txt", "w") as text:
+        text.write("This is a test!\n Did it work??\n I think it did!\n")
+
+
+writeBar()
